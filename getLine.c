1a6b2ed83ad6ef983d5b0115384232a4daa48207
Justice Merrick <106972535+justicemerrick@users.noreply.github.com>
2022-09-09 01:36:28 +0300

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained command
+ * input_buf - buffers chained commands
  * @info: parameter struct
  * @buf: address of buffer
  * @len: address of len var
